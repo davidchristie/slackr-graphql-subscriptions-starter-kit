5e5bab2f01331107751327ce5c8490585f83b0e6
David Christie <davidchristie.nz@gmail.com>
2017-05-20 13:35:26 +1200

    Refactor to react-scripts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { render } from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import { browserHistory, Route, Router } from 'react-router'
 
 import App from './components/App'
@@ -14,37 +13,15 @@ import makeApolloClient from './utilities/makeApolloClient'
 const client = makeApolloClient(config.scapholdUrl)
 
 const root = (
-  <AppContainer>
-    <ApolloProvider client={client}>
-      <Router history={browserHistory}>
-        <Route path='/' component={App}>
-          <Route path='login' component={Login} />
-          <Route path='createChannel' component={CreateChannel} />
-          <Route path='/channels/:channelId' component={Messages} />
-        </Route>
-      </Router>
-    </ApolloProvider>
-  </AppContainer>
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <Route path='login' component={Login} />
+        <Route path='createChannel' component={CreateChannel} />
+        <Route path='/channels/:channelId' component={Messages} />
+      </Route>
+    </Router>
+  </ApolloProvider>
 )
 
-render(root, document.querySelector('#app'))
-
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    const App = require('./components/App').default
-    render(
-      <AppContainer>
-        <ApolloProvider client={client}>
-          <Router history={browserHistory}>
-            <Route path='/' component={App}>
-              <Route component={Login} path='login' />
-              <Route component={Messages} path='/channels/:channelId' />
-            </Route>
-            <Route path='/createChannel' component={CreateChannel} />
-          </Router>
-        </ApolloProvider>
-      </AppContainer>,
-      document.querySelector('#app')
-    )
-  })
-}
+render(root, document.querySelector('#root'))
